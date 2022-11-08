554cd97fd78719aa52f0a7126dd526813ec80050
oli <olessia.potapova@gmail.com>
2022-11-08 10:43:36 +0800

    fixed formatting

--- src/components/MultipleChoice.tsx
+++ src/components/MultipleChoice.tsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const MultipleChoice = (props: Props) => {
     const [answerStatus, setAnswerStatus] = useState(true);
 
-
     const handleAnswer = (value: string) => {
         if (value === props.correctValue.toLowerCase()) {
-            setAnswerStatus(true)
+            setAnswerStatus(true);
         } else {
             setAnswerStatus(false);
         }
-    }
+    };
     return (
         <>
-            <select onChange={(e) => handleAnswer(e.target.value)} style={{ border: answerStatus === undefined || answerStatus === true ? '2px solid green' : '2px solid red' }}>
+            <select
+                onChange={(e) => handleAnswer(e.target.value)}
+                style={{
+                    border: answerStatus === false ? "2px solid red" : "2px solid black",
+                }}
+            >
                 <option defaultValue="">___</option>
                 <option value="a">a</option>
                 <option value="an">an</option>
                 <option value="the">the</option>
             </select>
         </>
-    )
-}
+    );
+};
 
 interface Props {
     correctValue: string;
