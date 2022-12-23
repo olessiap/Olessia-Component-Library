68c7a08a6e1bb98edf1fb5e81c44b49a423dec59
oli <olessia.potapova@gmail.com>
2022-12-23 11:42:51 +0800

    VIP: component list composition setup

--- src/components/component-list/ComponentList.tsx
+++ src/components/component-list/ComponentList.tsx
@@ -3,10 +3,32 @@ import { ProgressBar } from '../progress-bar/ProgressBar';
 import './componentList.scss';
 
 export const ComponentList = () => {
+
+    const components = [
+        {
+            name: "Definite Indefinite Grammar Practice",
+            description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
+            image: "",
+            component: <DefiniteIndefinite />
+        },
+        {
+            name: "Progress Bar",
+            description: "A custom progress bar that displayes completed percentage number",
+            image: "",
+            component: <ProgressBar />
+        }
+    ]
     return (
         <div>
-            <DefiniteIndefinite />
-            <ProgressBar />
+            {/* <DefiniteIndefinite />
+            <ProgressBar /> */}
+            {components.map((item) => {
+                return (
+                    <div>
+                        <h1>{item.name}</h1>
+                    </div>
+                )
+            })}
         </div>
     );
 }
