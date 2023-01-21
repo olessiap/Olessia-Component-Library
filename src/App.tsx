de1e3569d8b737c7307c27c632abdfedd63cb528
oli <olessia.potapova@gmail.com>
2023-01-21 14:34:50 -0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
+import { useState } from 'react';
 import './App.scss';
 import { ComponentList } from './components/component-list/ComponentList';
+import { DefiniteIndefinite } from './components/definite-indefinite/definiteIndefinite';
+import { ProgressBar } from './components/progress-bar/ProgressBar';
 
 
 const App = () => {
+  const [showComponent, setShowComponent] = useState(false);
+  const components = [
+    {
+      name: "Definite Indefinite Grammar Practice",
+      description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
+      image: "./images/definite-indefinite.jpg",
+      component: <DefiniteIndefinite />
+    },
+    {
+      name: "Progress Bar",
+      description: "A custom progress bar that displays a completed percentage number",
+      image: "./images/cat.jpeg",
+      component: <ProgressBar />
+    },
+  ]
+
   return (
-    <div>
-      <ComponentList />
-    </div>
+    <>
+      {showComponent ? (
+        <>
+          component here
+        </>
+      ) : (
+        <div>
+          <ComponentList onClick={() => setShowComponent(true)} items={components} />
+        </div>
+
+      )}
+    </>
   );
 }
 
--- src/components/component-list/ComponentList.tsx
+++ src/components/component-list/ComponentList.tsx
@@ -3,32 +3,37 @@ import { ProgressBar } from '../progress-bar/ProgressBar';
 import { ComponentItem } from './ComponentItem';
 import './componentList.scss';
 
-export const ComponentList = () => {
+export const ComponentList = (props: Props) => {
 
-    const components = [
-        {
-            name: "Definite Indefinite Grammar Practice",
-            description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
-            image: "./images/definite-indefinite.jpg",
-            component: <DefiniteIndefinite />
-        },
-        {
-            name: "Progress Bar",
-            description: "A custom progress bar that displays a completed percentage number",
-            image: "./images/cat.jpeg",
-            component: <ProgressBar />
-        },
-    ]
+    // const components = [
+    //     {
+    //         name: "Definite Indefinite Grammar Practice",
+    //         description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
+    //         image: "./images/definite-indefinite.jpg",
+    //         component: <DefiniteIndefinite />
+    //     },
+    //     {
+    //         name: "Progress Bar",
+    //         description: "A custom progress bar that displays a completed percentage number",
+    //         image: "./images/cat.jpeg",
+    //         component: <ProgressBar />
+    //     },
+    // ]
     return (
         <>
             <h1 className="heading">Olessia's Component Library List</h1>
             <div className="card-container">
-                {components.map((item) => {
+                {props.items.map((item: any) => {
                     return (
-                        <ComponentItem item={item} />
+                        <ComponentItem item={item} onClick={(item) => props.onClick(item)} />
                     )
                 })}
             </div>
         </>
     );
+}
+
+interface Props {
+    items: any;
+    onClick: (item: any) => void;
 }
