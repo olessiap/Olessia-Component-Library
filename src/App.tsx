f824e6f234d5b9288066bf74cef657f4655ccc52
oli <olessia.potapova@gmail.com>
2023-01-23 22:09:55 -0800

    refactored component list into its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 import { ComponentList } from './components/component-list/ComponentList';
-import { DefiniteIndefinite } from './components/definite-indefinite/definiteIndefinite';
-import { ProgressBar } from './components/progress-bar/ProgressBar';
+import { listOfCompontents } from './components/component-list/listOfComponents';
 import './App.scss';
 
-
 const App = () => {
-  const components = [
-    {
-      name: "Definite Indefinite Grammar Practice",
-      description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
-      image: "./images/definite-indefinite.jpg",
-      component: <DefiniteIndefinite />
-    },
-    {
-      name: "Progress Bar",
-      description: "A custom progress bar that displays a completed percentage number",
-      image: "./images/cat.jpeg",
-      component: <ProgressBar />
-    },
-  ]
 
   return (
     <div>
-      <ComponentList items={components} />
+      <ComponentList items={listOfCompontents} />
     </div>
   );
 }
 
 export default App;
-
-// TODO 
-// onClick re-route to new route and component UI
-// Switch Between Mason Grid And Slider View
-// generate new image based on key word
-//create reusable button component
-//fix styles
---
+++ src/components/component-list/listOfComponents.tsx
@@ -0,0 +1,17 @@
+import { DefiniteIndefinite } from "../definite-indefinite/definiteIndefinite";
+import { ProgressBar } from "../progress-bar/ProgressBar";
+
+export const listOfCompontents = [
+  {
+    name: "Definite Indefinite Grammar Practice",
+    description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
+    image: "./images/definite-indefinite.jpg",
+    component: <DefiniteIndefinite />
+  },
+  {
+    name: "Progress Bar",
+    description: "A custom progress bar that displays a completed percentage number",
+    image: "./images/cat.jpeg",
+    component: <ProgressBar />
+  },
+]
