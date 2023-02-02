1039a2f7a621745df8802a13b69a070de8496968
oli <olessia.potapova@gmail.com>
2023-02-02 12:54:55 -0800

    rendering multiple accordions with their own open/closed states

--- src/components/accordion1/Accordion1.tsx
+++ src/components/accordion1/Accordion1.tsx
@@ -2,25 +2,47 @@ import { useState } from 'react';
 import './accordion1.scss';
 
 export const Accordion1 = () => {
+
+    const accordionData = [
+        {
+            'title': "HTML",
+            'content': "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+        },
+        {
+            "title": "CSS",
+            'content': "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML",
+        }
+    ]
+
+    return (
+        <>
+            {accordionData.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <AccordionContent title={item.title} content={item.content} />
+                    </div>
+                )
+            })}
+        </>
+    );
+}
+
+const AccordionContent = (props: Props) => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div>
-            <div>
-                HTML{' '}
-                <span
-                    aria-hidden={true}
-                    onClick={() => setIsOpen(!isOpen)}
-                    className={['accordion-icon', isOpen && 'accordion-icon--filed'].filter(Boolean).join(' ')}
-                />
-            </div>
-            {isOpen && (
-                <div>
-                    The HyperText Markup Language or HTML is the
-                    standard markup language for documents designed to
-                    be displayed in a web browser.
-                </div>
+        <>
+            {props.title}{' '}
+            <span
+                aria-hidden={true}
+                onClick={() => setIsOpen(!isOpen)}
+                className={['accordion-icon', isOpen && 'accordion-icon--filed'].filter(Boolean).join(' ')}
+            ></span>
+            <div>{isOpen && props.content}</div>
+        </>
+    )
 
-            )}
-        </div>
-    );
+}
+interface Props {
+    title: string;
+    content: string;
 }
