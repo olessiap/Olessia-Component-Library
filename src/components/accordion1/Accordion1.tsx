7a5ccab0614ddc1c30534951452b638f5155e6d2
oli <olessia.potapova@gmail.com>
2023-02-09 12:01:29 -0800

    accordion data passed in as prop

--- src/components/accordion1/Accordion1.tsx
+++ src/components/accordion1/Accordion1.tsx
@@ -1,58 +1,71 @@
 import { useState } from 'react';
 import './accordion1.scss';
 
-export const Accordion1 = () => {
-
-    const accordionData = [
-        {
-            'title': "HTML",
-            'content': "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
-        },
-        {
-            "title": "CSS",
-            'content': "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML",
-        }
-    ]
+export const Accordion1 = (props: Props) => {
+    const [openSections, setOpenSections] = useState(
+        new Set(),
+    );
+    const [count, setCount] = useState();
 
     return (
         <div>
-            {accordionData.map((item, index) => {
+            {props.accordionData.map((item: any, index: number) => {
+                const isExpanded = openSections.has(item.value);
                 return (
-                    <div key={index}>
-                        <AccordionContent title={item.title} content={item.content} />
+                    <div key={index} className="accordion">
+                        <div className="accordion-item">
+                            <button
+                                onClick={() => {
+                                    const newOpenSections = new Set(openSections);
+                                    newOpenSections.has(item.value) ? newOpenSections.delete(item.value) : newOpenSections.add(item.value);
+                                    setOpenSections(newOpenSections)
+                                }}
+                                className="accordion-item-title">
+                                {item.title}{' '}
+                                <span
+                                    aria-hidden={true}
+
+                                    className={['accordion-icon', isExpanded && 'accordion-icon--filed'].filter(Boolean).join(' ')}
+                                />
+                            </button>
+                            <div
+                                hidden={!isExpanded}
+                                className="accordion-item-contents">
+                                {item.content}
+                            </div>
+                        </div>
                     </div>
                 )
             })}
         </div>
     );
 }
-
-const AccordionContent = (props: Props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <div className="accordion">
-            <div className="accordion-item">
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="accordion-item-title">
-                    {props.title}{' '}
-                    <span
-                        aria-hidden={true}
-
-                        className={['accordion-icon', isOpen && 'accordion-icon--filed'].filter(Boolean).join(' ')}
-                    />
-                </button>
-                <div
-                    hidden={!isOpen}
-                    className="accordion-item-contents">
-                    {props.content}
-                </div>
-            </div>
-        </div>
-    )
-
-}
 interface Props {
-    title: string;
-    content: string;
+    accordionData: any;
 }
+
+// const AccordionContent = (props: Props) => {
+//     const [isOpen, setIsOpen] = useState(false);
+//     return (
+//         <div className="accordion">
+//             <div className="accordion-item">
+//                 <button
+//                     onClick={() => setIsOpen(!isOpen)}
+//                     className="accordion-item-title">
+//                     {props.title}{' '}
+//                     <span
+//                         aria-hidden={true}
+
+//                         className={['accordion-icon', isOpen && 'accordion-icon--filed'].filter(Boolean).join(' ')}
+//                     />
+//                 </button>
+//                 <div
+//                     hidden={!isOpen}
+//                     className="accordion-item-contents">
+//                     {props.content}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+
+// }
--- src/components/component-list/listOfComponents.tsx
+++ src/components/component-list/listOfComponents.tsx
@@ -1,9 +1,29 @@
 import { Accordion1 } from "../accordion1/Accordion1";
 import { DefiniteIndefinite } from "../definite-indefinite/definiteIndefinite";
+import { GeneratedTable } from "../generated-table/GeneratedTable";
 import { ProgressBarParent } from "../progress-bar/ProgressBarParent";
 import { ProgressBarsParent } from "../progress-bar/ProgressBarsParent";
 
+const accordion1Data = [
+  {
+    'value': 'html',
+    'title': "HTML",
+    'content': "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  },
+  {
+    "value": "css",
+    "title": "CSS",
+    'content': "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML",
+  }
+]
+
 export const listOfCompontents = [
+  {
+    name: "Generated Table",
+    description: "Generates a table of numbers given the rows and columns from user input",
+    image: "./images/cat.jpeg",
+    component: <GeneratedTable />
+  },
   {
     name: "Definite Indefinite Grammar Practice",
     description: "This is a multiple choice grammar quiz that tests the user's knowledge of articles",
@@ -26,6 +46,6 @@ export const listOfCompontents = [
     name: "Accordion 1",
     description: "Accordion component that displays a list of vertically stacked sections that each contain a title and content snippet.",
     image: "./images/cat.jpeg",
-    component: <Accordion1 />
+    component: <Accordion1 accordionData={accordion1Data} />
   },
 ]
