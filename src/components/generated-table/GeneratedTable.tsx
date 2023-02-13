98eedb4125cab13a9ad329fec20a6cf47bbbc88a
oli <olessia.potapova@gmail.com>
2023-02-13 15:18:36 -0800

    refactored to generate the table with <table>

--- src/components/generated-table/GeneratedTable.tsx
+++ src/components/generated-table/GeneratedTable.tsx
@@ -2,38 +2,76 @@ import { useState } from 'react';
 import './generatedTable.scss';
 
 export const GeneratedTable = () => {
-    const [rows, setRows] = useState('');
-    const [columns, setColumns] = useState('');
-    const [numberOfCells, setNumberOfCells] = useState(0);
+    const [rows, setRows] = useState(0);
+    const [columns, setColumns] = useState(0);
 
-    function handleSubmit(e: { preventDefault: () => void; }) {
+    function handleSubmit(e: any) {
         e.preventDefault()
-        setNumberOfCells(Number(rows) * Number(columns));
+        const data = new FormData(e.target);
+        const rows = data.get("rows");
+        setRows(Number(rows));
+        const columns = data.get('columns');
+        setColumns(Number(columns));
     }
+
+    console.log('rows: ', rows)
+    console.log('Boolean(rows)', Boolean(rows));
 
     return (
         <div className="main-container">
-            <form onSubmit={handleSubmit} className="form">
-                <label>
-                    Rows
-                    <input type="number" value={rows} onChange={(e) => setRows(e.target.value)} className="input" />
-                </label>
-                <label>
-                    Columns
-                    <input type="number" value={columns} onChange={(e) => setColumns(e.target.value)} className="input" />
-                </label>
-                <input type="submit" value="Submit" className="submit-button" />
+            <form onSubmit={(e) => handleSubmit(e)} className="form">
+                <label htmlFor="Rows">Rows</label>
+                <input
+                    id="rows"
+                    name="rows"
+                    type="number"
+                    min={1}
+                    defaultValue={rows}
+                    className="input" />
+                <label htmlFor="Columns">Columns</label>
+                <input
+                    id="columns"
+                    name="columns"
+                    type="number"
+                    min={1}
+                    defaultValue={columns}
+                    className="input" />
+                <button type="submit" className="submit-button">Submit</button>
             </form>
-            <div
-                className="table-container"
-                style={{ gridTemplateColumns: `repeat(${columns}, 40px)` }}
-            >
-                {Array(numberOfCells).fill(null).map((_, index) => {
-                    return (
-                        <div className="cell" key={index}>{index + 1}</div>
-                    )
-                })}
-            </div>
+            {Boolean(rows) && Boolean(columns) && (
+                <Table rows={rows} columns={columns} />
+
+            )}
         </div>
     );
+}
+
+const Table = (props: Props) => {
+    return (
+        <table>
+            <tbody>
+                {Array.from({ length: props.rows }, () => 0).map(
+                    (_, row) => (
+                        <tr key={row}>
+                            {Array.from({ length: props.columns }, () => 0).map(
+                                (_, col) => (
+                                    <td key={col} className="cell">
+                                        {col % 2 === 0
+                                            ? props.rows * col + (row + 1)
+                                            : props.rows * (col + 1) - row}
+                                    </td>
+                                ),
+                            )}
+                        </tr>
+                    ),
+                )}
+            </tbody>
+
+        </table>
+    )
+}
+
+interface Props {
+    rows: number;
+    columns: number;
 }
