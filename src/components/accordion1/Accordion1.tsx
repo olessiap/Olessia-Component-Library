69f042878341c947693f0996d8be28c789fe8667
oli <olessia.potapova@gmail.com>
2023-03-23 14:07:12 +0800

    added keyboard events

--- src/components/accordion1/Accordion1.tsx
+++ src/components/accordion1/Accordion1.tsx
@@ -15,6 +15,19 @@ export const Accordion1 = (props: Props) => {
         new Set(),
     );
 
+    function focusOnSection(index: number) {
+        if (window.document !== null) {
+            document
+                .getElementById(
+                    getAccordionHeaderId(
+                        accordionId,
+                        props.accordionData[index].value,
+                    ),
+                )!
+                .focus();
+        }
+    }
+
     return (
         <div className="accordion-container">
             {props.accordionData.map((item: any, index: number) => {
@@ -23,7 +36,50 @@ export const Accordion1 = (props: Props) => {
                 const panelId = getPanelAccordionId(accordionId, item.value);
 
                 return (
-                    <div key={index} className="accordion">
+                    <div
+                        key={index}
+                        className="accordion"
+                        onKeyDown={(event) => {
+                            console.log('clicked on this event ', event);
+                            const activeItemValue =
+                                document.activeElement && document.activeElement.getAttribute("data-accordion-value");
+                            ;
+                            if (activeItemValue == null) {
+                                return;
+                            }
+
+                            switch (event.code) {
+                                case 'ArrowUp': {
+                                    const index = props.accordionData.findIndex(
+                                        (value: string) =>
+                                            value === activeItemValue,
+                                    );
+                                    focusOnSection(
+                                        (index - 1 + props.accordionData.length) %
+                                        props.accordionData.length,
+                                    );
+                                    break;
+                                }
+                                case 'ArrowDown': {
+                                    const index = props.accordionData.findIndex(
+                                        (value: string) =>
+                                            value === activeItemValue,
+                                    );
+                                    focusOnSection((index + 1) % props.accordionData.length);
+                                    break;
+                                }
+                                case 'Home': {
+                                    focusOnSection(0);
+                                    break;
+                                }
+                                case 'End': {
+                                    focusOnSection(props.accordionData.length - 1);
+                                    break;
+                                }
+                                default:
+                                    break;
+                            }
+                        }}>
                         <div className="accordion-item">
                             <button
                                 onClick={() => {
@@ -34,6 +90,8 @@ export const Accordion1 = (props: Props) => {
                                 className="accordion-item-title"
                                 aria-expanded={isExpanded}
                                 id={headerId}
+                                type="button"
+                                data-accordion-value={item.value}
                                 aria-controls={panelId}>
                                 {item.title}{' '}
                                 <span
