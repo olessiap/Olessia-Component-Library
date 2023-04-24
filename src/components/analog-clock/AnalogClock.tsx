2250e6cda25c5d6fd3aee5a1b47992ce0e7aed59
oli <olessia.potapova@gmail.com>
2023-04-24 11:33:13 +0800

    wip: setup current time analog

--- src/components/analog-clock/AnalogClock.tsx
+++ src/components/analog-clock/AnalogClock.tsx
@@ -1,9 +1,34 @@
 import './analogclock.scss';
+import { useEffect, useState } from 'react';
 
 export const AnalogClock = () => {
+    const [date, setDate] = useState(new Date());
+    const [hour, setHour] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [second, setSecond] = useState(0);
+
+    useEffect(() => {
+        console.log(new Date());
+        const today = new Date();
+        const hour = today.getHours();
+        const minute = today.getMinutes();
+        const second = today.getSeconds();
+
+        setHour(hour * 30);
+        setMinute(minute * 6);
+        setSecond(second * 6);
+    }, [])
+
+
     return (
         <div>
-            Analog clock will be here
+            <div className="clock-outer">
+                <div className="clock-inner">
+                    <div className="hand hour" style={{ transform: `rotate(${hour}deg)` }}></div>
+                    <div className="hand minute" style={{ transform: `rotate(${minute}deg)` }}></div>
+                    <div className="hand second" style={{ transform: `rotate(${second}deg)` }}></div>
+                </div>
+            </div>
         </div>
     );
 }
